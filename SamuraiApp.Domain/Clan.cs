60d532b2e9dd3242a1b821214faf78fb0a710721
Wael-Yassir <wael.yassir@gmail.com>
2022-05-16 20:56:36 +0200

    Add project files.

---
+++ SamuraiApp.Domain/Clan.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace SamuraiApp.Domain
+{
+    public class Clan
+    {
+        public int Id { get; set; }
+        public string ClanName { get; set; }
+    }
+}
