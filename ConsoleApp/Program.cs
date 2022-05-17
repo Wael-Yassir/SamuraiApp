9765bba85f329dc21730149323fb04e3c6b8861d
Wael-Yassir <wael.yassir@gmail.com>
2022-05-17 21:49:15 +0200

    Add db context class

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Linq;
+using SamuraiApp.Data;
+using SamuraiApp.Domain;
 
 namespace ConsoleApp
 {
     internal class Program
     {
+        private static SamuraiContext context = new SamuraiContext();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            context.Database.EnsureCreated();
+            GetSamurais("Before Add:");
+            AddSamurai();
+            GetSamurais("After Add:");
+
+            Console.WriteLine("Press any key ... ");
+            Console.ReadKey();
+        }
+
+        private static void AddSamurai()
+        {
+            var samurai = new Samurai { Name = "Sampson" };
+            context.Samurais.Add(samurai);
+            context.SaveChanges();
+        }
+
+        private static void GetSamurais(string text)
+        {
+            var samurais = context.Samurais.ToList();
+            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach (var samura in samurais)
+            {
+                Console.WriteLine(samura.Name);
+            }
         }
     }
 }
---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using SamuraiApp.Domain;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
+        }
+    }
+}
